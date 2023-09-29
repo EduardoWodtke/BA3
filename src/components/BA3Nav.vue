1228631972370216c6ea91095468c8e95d418e39
EduardoWodtke <eduardorwodtkeifc@gmail.com>
2023-09-28 22:31:47 -0300

    a

--- src/components/BA3Nav.vue
+++ src/components/BA3Nav.vue
@@ -23,15 +23,13 @@
         <img src="../assets/lancados.png" alt="">
       </div>
       <div>
-        <img
-          src="https://th.bing.com/th/id/R.0aed9c7a5752863d4ba49539dd072a48?rik=%2b5QnOU08U670FA&riu=http%3a%2f%2fclipart-library.com%2fimages_k%2fwhite-heart-png-transparent%2fwhite-heart-png-transparent-3.png&ehk=gERPhhaF6yO68%2bKAfdwN3fjnoAo8Xu32sLY8c3vH5Y0%3d&risl=&pid=ImgRaw&r=0"
-          alt="favoritos">
+        <img src="" alt="favoritos">
       </div>
       <div>
         <img src="https://o.remove.bg/downloads/ad13a6c7-ace5-45b8-aaf8-d8e40f39820c/OIP-removebg-preview.png" alt="assitri mais tarde">
       </div>
       <div>
-        <img src="https://th.bing.com/th/id/R.b97a1aa3a7e1c62de58febd76728773c?rik=5eVVqkJwrfqfMQ&riu=http%3a%2f%2fwww.icons101.com%2ficon_png%2fsize_512%2fid_67747%2fLaunchpad01.png&ehk=k0ZbrEqId82zS8xIapSReMOfZ%2fsTpUleceXP0OIyJBU%3d&risl=&pid=ImgRaw&r=0" alt="">
+        <img src="../assets/R.png" alt="">
       </div>
     </div>
   </nav>
@@ -66,8 +64,8 @@ nav{
   background-color: rgb(0, 0, 0); 
   height: 100%;
   overflow: hidden;
-  /* border-right: 2px solid rgb(65, 65, 65);
- */
+  /*border-right: 2px solid rgb(65, 65, 65);*/
+ 
 }
 
 #conteiner img {
